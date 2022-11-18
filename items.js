120063a1e6ede52ab6c78d0814275e835e46bda9
duaidrees <116306775+duaidrees@users.noreply.github.com>
2022-11-18 14:59:53 +0000

    Add files via upload

---
+++ items.js
@@ -0,0 +1,14 @@
+let items = [
+    {id:01, subject: 'Math', location: 'London',price: "100",spaces: 10,image: 'img/icon.png'},
+    {id:02, subject: 'English', location: 'Bristol',price: "120",spaces: 5,image: 'img/eng.png'},
+    {id:03, subject: 'Music', location: 'Oxford',price: "130",spaces: 8,image: 'img/music.png'},
+    {id:04, subject: 'Science', location: 'London',price: "150",spaces: 6,image: 'img/sci.png'},
+    {id:05, subject: 'History', location: 'Oxford',price: "110",spaces: 9,image: 'img/hist.png'},
+    {id:06, subject: 'Math', location: 'Bristol',price: "160",spaces: 9,image: 'img/icon.png'},
+    {id:07, subject: 'Math', location: 'London',price: "170",spaces: 10,image: 'img/icon.png'},
+    {id:08, subject: 'English', location: 'Oxford',price: "115",spaces: 5,image: 'img/eng.png'},
+    {id:09, subject: 'Music', location: 'London',price: "105",spaces: 7,image: 'img/music.png'},
+    {id:10, subject: 'Science', location: 'Leeds',price: "165",spaces: 8,image: 'img/sci.png'},
+    {id:11, subject: 'History', location: 'London',price: "185",spaces: 6,image: 'img/hist.png'},
+    {id:12, subject: 'CS', location: 'Leeds',price: "190",spaces: 9,image: 'img/icon.png'},
+]
